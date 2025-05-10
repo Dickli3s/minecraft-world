c156da45bd0e3d726bf213bf27f71c115195dddf
sq3ka <eolson1cc@gmail.com>
2025-05-10 16:28:52 +0300

    Added components : ground, block, scene

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import React from "react";
+import Scene from './Components/Scene';
+import { Canvas } from '@react-three/fiber';
+import { positionView } from 'three/src/nodes/TSL.js';
+
 
 function App() {
   return (
     <div className="App">
-    
+    <h1>Minecraft-World</h1>
+    <Canvas
+    shadows
+    camera={{ position: [5, 10, 10], fov: 70 }}
+    style={{ width: '100vw', height: '100vw', background: '#B3E5FC'}}
+    >
+    <Scene/>
+    </Canvas>
     </div>
   );
 }
